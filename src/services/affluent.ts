274f94335ee4b07aacdac1e7e719548877261c1e
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2020-11-03 12:56:41 +0300

    - refactor affluent

--- src/services/affluent.ts
+++ src/services/affluent.ts
@@ -28,7 +28,7 @@ const Service: IBaseService<IAffluentData> = {
       '--window-position=0,0',
       '--ignore-certifcate-errors',
       '--ignore-certifcate-errors-spki-list',
-      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
     ];
 
     const browser = await puppeteer.launch({
@@ -62,7 +62,6 @@ const Service: IBaseService<IAffluentData> = {
 
     await clickWithWait(page, '#DataTables_Table_0_length .dropdown-toggle');
 
-    // await page.click('#DataTables_Table_0_length ul.dropdown-menu li:last-child');
     await clickWithWait(page, '#DataTables_Table_0_length ul.dropdown-menu li:last-child');
 
     await page.waitForResponse('https://develop.pub.afflu.net/api/query/dates');
