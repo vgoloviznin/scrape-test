da55dcfe6a25bae015ef600455ee4e03b84b40b1
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2020-11-04 23:36:54 +0300

    - getting user data for any number of pages

--- src/services/req-res.ts
+++ src/services/req-res.ts
@@ -3,12 +3,23 @@ import knex from '../helpers/knex';
 
 const Service: IBaseService<IUserObject> = {
   scrape: async (): Promise<IUserObject[]> => {
-    const [firstPage, secondPage] = await Promise.all([
-      axios.get<IReqResResponse>('https://reqres.in/api/users?page=1'),
-      axios.get<IReqResResponse>('https://reqres.in/api/users?page=2'),
-    ]);
+    const firstResponse = await axios.get<IReqResResponse>('https://reqres.in/api/users?page=1');
 
-    const users = firstPage.data.data.concat(secondPage.data.data);
+    const users = firstResponse.data.data;
+
+    if (firstResponse.data.total_pages > 1) {
+      const proms = Array.from({ length: firstResponse.data.total_pages - 1 }).map(async (_, i) => {
+        const page = await axios.get<IReqResResponse>(`https://reqres.in/api/users?page=${i + 2}`);
+
+        return page.data.data;
+      });
+
+      const otherPages = await Promise.all(proms);
+
+      const combined = users.concat(...otherPages);
+
+      return combined;
+    }
 
     return users;
   },
