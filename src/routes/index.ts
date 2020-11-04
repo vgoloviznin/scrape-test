bc469aa096774105386fe1818adbb0c4e3a1b5a7
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2020-11-04 23:18:27 +0300

    - ajax loading data - render data with tabulator

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,16 +15,20 @@ router.get('/reload', async (ctx: koa.Context) => {
   ctx.redirect('/');
 });
 
-router.get('/', async (ctx: koa.Context) => {
-  const [users, affluent] = await Promise.all([
-    ReqResService.getAll(),
-    AffluentService.getAll(),
-  ]);
+router.get('/users', async (ctx: koa.Context) => {
+  const users = await ReqResService.getAll();
 
-  await ctx.render('content', {
-    users,
-    affluent,
-  });
+  ctx.body = users;
+});
+
+router.get('/affluent', async (ctx: koa.Context) => {
+  const affluent = await AffluentService.getAll();
+
+  ctx.body = affluent;
+});
+
+router.get('/', async (ctx: koa.Context) => {
+  await ctx.render('content');
 });
 
 export default router;
